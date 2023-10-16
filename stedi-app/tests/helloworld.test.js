92538e3adaec6a191318dcca8f8c0265877aebd2
lg10migue <lop18028@byui.edu>
2023-10-15 19:42:47 -0600

    Add a new function in the helloworld file.
    Added the function sendName and tests for it to test this function automatically.

--- stedi-app/tests/helloworld.test.js
+++ stedi-app/tests/helloworld.test.js
@@ -1,9 +1,16 @@
-import { sendHello } from "../utils/helloworld";
+import { sendHello, sendName } from "../utils/helloworld";
 import assert from "assert" ;
 
 // Manual testing.
-
 it( "Testing sendHello function", () => {
     const hello = sendHello();
     assert.equal( hello, "Hello!" );
+} );
+
+// Automated Unit Tests.
+it( "Should say Miguel", () => {
+    const myName = sendName();
+    console.log( myName );
+
+    expect( myName ).toBe( "Miguel" );
 } );
--- stedi-app/utils/helloworld.js
+++ stedi-app/utils/helloworld.js
@@ -1,3 +1,7 @@
 export const sendHello = ()=> {
     return "Hello!";
 };
+
+export const sendName = () => {
+    return "Miguel";
+};
